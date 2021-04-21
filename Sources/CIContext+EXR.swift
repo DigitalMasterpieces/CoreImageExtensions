75ab14fcc4ef726253d6b9af1ac43c2c832170ab
Frank Schlegel <frank.schlegel@digitalmasterpieces.com>
2021-04-21 09:50:46 +0200

    Throw error instead of assertions.

--- Sources/CIContext+EXR.swift
+++ Sources/CIContext+EXR.swift
@@ -3,6 +3,14 @@ import CoreImage
 
 public extension CIContext {
 
+    /// Errors that might be caused during image export.
+    enum ExportError: Error {
+        case unsupportedExtent(message: String)
+        case renderingFailure(message: String)
+        case imageDestinationCreationFailure(message: String)
+    }
+
+
     /// Renders the image and exports the resulting image data in EXR format.
     ///
     /// To render an image for export, the image’s contents must not be empty and its extent dimensions must be finite.
@@ -17,28 +25,25 @@ public extension CIContext {
     ///   - image: The image to render.
     ///   - format: The pixel format for the output image.
     ///   - colorSpace: The color space in which to render the output image. This color space must conform
-    ///                 to either the CGColorSpaceModel.rgb or CGColorSpaceModel.monochrome model and must be compatible
+    ///                 to either the `CGColorSpaceModel.rgb` or `CGColorSpaceModel.monochrome` model and must be compatible
     ///                 with the specified pixel format.
     ///   - options: No options keys are supported at this time.
-    /// - Returns: A data representation of the rendered image in EXR format, or nil if the image could not be rendered.
-    func exrRepresentation(of image: CIImage, format: CIFormat, colorSpace: CGColorSpace?, options: [CIImageRepresentationOption: Any] = [:]) -> Data? {
+    /// - Returns: A data representation of the rendered image in EXR format.
+    /// - Throws: A `CIContext.ExportError` if the image data could not be created.
+    func exrRepresentation(of image: CIImage, format: CIFormat, colorSpace: CGColorSpace?, options: [CIImageRepresentationOption: Any] = [:]) throws -> Data {
         guard image.extent.height >= 16 else {
-            assertionFailure("The image's height must be at least 16 due to a bug in Apple's EXR encoder implementation")
-            return nil
+            throw ExportError.unsupportedExtent(message: "The image's height must be at least 16 due to a bug in Apple's EXR encoder implementation")
         }
         guard !image.extent.isInfinite else {
-            assertionFailure("The image's extent must be finite")
-            return nil
+            throw ExportError.unsupportedExtent(message: "The image's extent must be finite")
         }
         guard let cgImage = self.createCGImage(image, from: image.extent, format: format, colorSpace: colorSpace) else {
-            assertionFailure("Failed to render image")
-            return nil
+            throw ExportError.renderingFailure(message: "Failed to render image")
         }
 
         let data = NSMutableData()
         guard let destination = CGImageDestinationCreateWithData(data, "com.ilm.openexr-image" as CFString, 1, nil) else {
-            assertionFailure("Failed to create an EXR image destination")
-            return nil
+            throw ExportError.imageDestinationCreationFailure(message: "Failed to create an EXR image destination")
         }
         CGImageDestinationAddImage(destination, cgImage, image.properties as CFDictionary)
         CGImageDestinationFinalize(destination)
--- Tests/EXRTests.swift
+++ Tests/EXRTests.swift
@@ -10,21 +10,22 @@ class EXRTests: XCTestCase {
     func testEXRDataCreation() {
         let testImage = CIImage.containing(values: CIVector(x: -2.0, y: 0.0, z: 3.0, w: 1.0))!.cropped(to: CGRect(x: 0, y: 0, width: 32, height: 16))
 
-        guard let exrData = self.context.exrRepresentation(of: testImage, format: .RGBAh, colorSpace: context.workingColorSpace ?? CGColorSpaceCreateDeviceRGB()) else {
-            XCTFail("Failed to create EXR data from image")
-            return
-        }
+        do {
+            let exrData = try self.context.exrRepresentation(of: testImage, format: .RGBAh, colorSpace: context.workingColorSpace ?? CGColorSpaceCreateDeviceRGB())
 
-        guard let loadedImage = CIImage(data: exrData) else {
-            XCTFail("Failed to read EXR data back into image")
-            return
-        }
+            guard let loadedImage = CIImage(data: exrData) else {
+                XCTFail("Failed to read EXR data back into image")
+                return
+            }
 
-        let value = self.context.readFloat32PixelValue(from: loadedImage, at: .zero)
-        XCTAssertEqual(value.r, -2.0, accuracy: 0.001)
-        XCTAssertEqual(value.g,  0.0, accuracy: 0.001)
-        XCTAssertEqual(value.b,  3.0, accuracy: 0.001)
-        XCTAssertEqual(value.a,  1.0, accuracy: 0.001)
+            let value = self.context.readFloat32PixelValue(from: loadedImage, at: .zero)
+            XCTAssertEqual(value.r, -2.0, accuracy: 0.001)
+            XCTAssertEqual(value.g,  0.0, accuracy: 0.001)
+            XCTAssertEqual(value.b,  3.0, accuracy: 0.001)
+            XCTAssertEqual(value.a,  1.0, accuracy: 0.001)
+        } catch {
+            XCTFail("Failed to create EXR data from image with error: \(error)")
+        }
     }
 
     func testEXRLoading() {
@@ -34,18 +35,19 @@ class EXRTests: XCTestCase {
         let value1 = self.context.readFloat32PixelValue(from: testEXRImage, at: CGPoint(x: 10, y: 20))
         XCTAssertEqual(value1.r, -3604.0)
 
-        // try to render the image back into data and load again
-        guard let exrData = self.context.exrRepresentation(of: testEXRImage, format: .RGBAh, colorSpace: context.workingColorSpace ?? CGColorSpaceCreateDeviceRGB()) else {
-            XCTFail("Failed to create EXR data from image")
-            return
-        }
-        guard let loadedImage = CIImage(data: exrData) else {
-            XCTFail("Failed to read EXR data back into image")
-            return
-        }
+        do {
+            // try to render the image back into data and load again
+            let exrData = try self.context.exrRepresentation(of: testEXRImage, format: .RGBAh, colorSpace: context.workingColorSpace ?? CGColorSpaceCreateDeviceRGB())
+            guard let loadedImage = CIImage(data: exrData) else {
+                XCTFail("Failed to read EXR data back into image")
+                return
+            }
 
-        let value2 = self.context.readFloat32PixelValue(from: loadedImage, at: CGPoint(x: 10, y: 20))
-        XCTAssertEqual(value2.r, -3604.0, accuracy: 0.001)
+            let value2 = self.context.readFloat32PixelValue(from: loadedImage, at: CGPoint(x: 10, y: 20))
+            XCTAssertEqual(value2.r, -3604.0, accuracy: 0.001)
+        } catch {
+            XCTFail("Failed to create EXR data from image with error: \(error)")
+        }
     }
 
 }
