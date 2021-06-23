64e716e46a14143437daa9f15c4f14bf3ac65bb6
Maik Zarnbach <maik.zarnbach@digitalmasterpieces.com>
2021-06-23 08:39:00 +0200

    Make the library dynamic.

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "CoreImageExtensions",
+            type: .dynamic,
             targets: ["CoreImageExtensions"]),
     ],
     targets: [
