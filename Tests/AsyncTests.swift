fd4202076a300057d5206e6885e5ab48e033b341
Frank Schlegel <frank.schlegel@digitalmasterpieces.com>
2022-01-13 07:45:59 +0100

    Added tests for async APIs.

---
+++ Tests/AsyncTests.swift
@@ -0,0 +1,34 @@
+import CoreImage
+import CoreImageExtensions
+import XCTest
+
+
+@available(iOS 13.0, macOS 10.15, macCatalyst 13.0, tvOS 13.0, *)
+class AsyncTests: XCTestCase {
+
+    let context = CIContext()
+
+    let testPixelImage = CIImage.containing(values: CIVector(x: 0.0, y: 0.5, z: 1.0, w: 1.0))!.cropped(to: .singlePixel)
+
+    func testForMemoryLeak() {
+        var context: CIContext? = CIContext()
+        // create actor
+        let _ = context?.async
+        // create weak reference
+        weak var weakContext = context
+        // release context
+        context = nil
+        XCTAssertNil(weakContext, "The context should release properly, even after creating the actor.")
+    }
+
+    func testReadPixelAsync() async {
+        let value = await self.context.async.readUInt8PixelValue(from: testPixelImage, at: .zero)
+        XCTAssertEqual(value, Pixel<UInt8>(x: 0, y: 128, z: 255, w: 255))
+    }
+
+    func testCreateCGImageAsync() async {
+        let cgImage = await self.context.async.createCGImage(testPixelImage, from: .singlePixel)
+        XCTAssertNotNil(cgImage)
+    }
+
+}
