23d74ebd0aa59fce263dde0086a1a0060afdbe54
Frank Rupprecht <frank@digitalmasterpieces.com>
2025-08-04 20:42:05 +0200

    Added `options` parameter to image lookup APIs.

--- Sources/CIImage+Lookup.swift
+++ Sources/CIImage+Lookup.swift
@@ -17,13 +17,14 @@ public extension CIImage {
     /// - Parameters:
     ///   - name: The name of the image. Should contain the file extension for bundle resources.
     ///   - bundle: The bundle containing the image file or asset catalog. Specify nil to search the app’s main bundle.
+    ///   - options: A dictionary that contains options for creating the image object.
     ///   - traitCollection: The traits associated with the intended environment for the image. Use this parameter to ensure
     ///                      that the correct variant of the image is loaded. If you specify nil,
     ///                      this method uses the traits associated with the main screen.
-    convenience init?(named name: String, in bundle: Bundle? = nil, compatibleWith traitCollection: UITraitCollection? = nil) {
+    convenience init?(named name: String, in bundle: Bundle? = nil, options: [CIImageOption: Any]? = nil, compatibleWith traitCollection: UITraitCollection? = nil) {
         // on iOS, UIImage handles all the lookup logic automatically, so just use that
         if let uiImage = UIImage(named: name, in: bundle, compatibleWith: traitCollection) {
-            self.init(image: uiImage)
+            self.init(image: uiImage, options: options)
         } else {
             return nil
         }
@@ -37,14 +38,15 @@ public extension CIImage {
     /// - Parameters:
     ///   - name: The name of the image. Should contain the file extension for bundle resources.
     ///   - bundle: The bundle containing the image file or asset catalog. Specify nil to search the app’s main bundle.
-    convenience init?(named name: String, in bundle: Bundle? = nil) {
+    ///   - options: A dictionary that contains options for creating the image object.
+    convenience init?(named name: String, in bundle: Bundle? = nil, options: [CIImageOption: Any]? = nil) {
         let bundle = bundle ?? Bundle.main
         // try to load from asset catalog first
         if let nsImage = bundle.image(forResource: name), let cgImage = nsImage.cgImage(forProposedRect: nil, context: nil, hints: nil) {
-            self.init(cgImage: cgImage)
+            self.init(cgImage: cgImage, options: options)
         // search the bundle directly otherwise
         } else if let url = bundle.url(forResource: name, withExtension: nil) {
-            self.init(contentsOf: url)
+            self.init(contentsOf: url, options: options)
         } else {
             return nil
         }
